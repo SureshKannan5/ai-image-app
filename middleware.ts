b5e89ed348ada2a72863b1fd66f4dbaa815d265e
sureshkannan5 <sureshkannan082@gmail.com>
2024-05-17 19:14:41 +0530

    fix: webhook fix

--- middleware.ts
+++ middleware.ts
@@ -7,9 +7,7 @@ const isProtectedRoute = createRouteMatcher([
   "/transformations(.*)",
 ]);
 
-export default clerkMiddleware((auth, req) => {
-  if (isProtectedRoute(req)) auth().protect();
-});
+export default clerkMiddleware();
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
